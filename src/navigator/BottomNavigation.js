e2b3238892fa6fd613f84e7871ed15a3d8d6caef
andkstrr <andikasatrionurcahyo@gmail.com>
2025-03-06 14:43:06 +0700

    add bottom navigation

--- src/navigator/BottomNavigation.js
+++ src/navigator/BottomNavigation.js
@@ -1,27 +1,87 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View, Text, Image} from 'react-native'
 import Reel from '../screen/dashboard/Reel'
 import Search from '../screen/dashboard/Search'
 import AddPost from '../screen/dashboard/AddPost'
 import Dashboard from '../screen/dashboard/Dashboard'
 import UserProfile from '../screen/dashboard/UserProfile'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 const Tab = createBottomTabNavigator()
 
 const BottomNavigation = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={Dashboard} />
-        <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="Add" component={AddPost} />
-        <Tab.Screen name="Reel" component={Reel} />
-        <Tab.Screen name="Profile" component={UserProfile} />
+    <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false}}>
+      <Tab.Screen
+        name="Home"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              style={{height: 24, width: 24}}
+              source={
+                focused
+                  ? require('../assets/footer/sHomeButton.png')
+                  : require('../assets/footer/homeButton.png')
+              }
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              style={{height: 24, width: 24}}
+              source={
+                focused
+                  ? require('../assets/footer/sSearch.png')
+                  : require('../assets/footer/search.png')
+              }
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AddPost"
+        component={AddPost}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              style={{height: 24, width: 24}}
+              source={require('../assets/footer/addPost.png')}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Reels"
+        component={Reel}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              style={{height: 24, width: 24}}
+              source={require('../assets/footer/reel.png')}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="UserProfile"
+        component={UserProfile}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              style={{height: 24, width: 24}}
+              source={require('../assets/footer/user.png')}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
 
 export default BottomNavigation
-
-// name = href
-// component = halaman yang dituju
--- src/navigator/StackNavigation.js
+++ src/navigator/StackNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Login from '../screen/auth/Login'
 import SignUp from '../screen/auth/SignUp'
+import { SafeAreaView } from 'react-native'
 import ViewStory from '../components/ViewStory'
 import BottomNavigation from './BottomNavigation'
 import Dashboard from '../screen/dashboard/Dashboard'
@@ -11,15 +12,17 @@ const Stack = createNativeStackNavigator()
 
 const StackNavigation = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Home" component={Dashboard} />
-        <Stack.Screen name="Story" component={ViewStory} />
-        <Stack.Screen name="Dashboard" component={BottomNavigation} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Home" component={Dashboard} />
+          <Stack.Screen name="Story" component={ViewStory} />
+          <Stack.Screen name="Dashboard" component={BottomNavigation} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 };
 
