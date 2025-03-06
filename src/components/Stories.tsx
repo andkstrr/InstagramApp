4daabb91b08c9cdebd056b2324295fc4efa40a19
andkstrr <andikasatrionurcahyo@gmail.com>
2025-03-06 13:15:30 +0700

    update flatlist stories

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,55 +1,58 @@
-import React from 'react'
-import { UsersData } from '../utils/UsersData'
-import { AppColor } from '../utils/AppColor'
-import { useNavigation } from '@react-navigation/native'
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
+import React from 'react';
+import { UsersData } from '../utils/UsersData';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
 
 const Stories = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   return (
     <View style={styles.storyContainer}>
-      {/* Map Data dari UsersData */}
-      {UsersData.map((item) => {
-        return (
-            <View key={item.id} style={{marginLeft: 10}}>
-              <TouchableOpacity onPress={() => {navigation.navigate('Story', {item})}}>
-                <View style={styles.storyImageContainer}>
-                    <Image source={item.profile} style={styles.storyImageProfile}></Image>
-                </View>
-              </TouchableOpacity>
-              <Text style={styles.storyUserName}>{item.username}</Text>
-            </View>
-        )
-      })}
+      <FlatList
+        data={UsersData}
+        keyExtractor={(item) => item.id.toString()} // Memastikan setiap item punya key unik
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={{ marginLeft: 10 }}>
+            <TouchableOpacity onPress={() => navigation.navigate('Story', { item })}>
+              <View style={styles.storyImageContainer}>
+                <Image source={item.profile} style={styles.storyImageProfile} />
+              </View>
+            </TouchableOpacity>
+            <Text style={styles.storyUserName}>
+              {item.username.length > 10 ? item.username.slice(0, 10) + '...' : item.username}
+            </Text>
+          </View>
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Stories
+export default Stories;
 
 const styles = StyleSheet.create({
-    storyContainer: {
-        flexDirection: 'row', 
-        marginTop: 10
-    },
-    storyImageContainer: {
-        borderWidth: 3,
-        borderRadius: 40,
-        borderColor: '#e5009f',
-        // borderColor: AppColor.GRADIENT,
-        // borderColor: linear-gradient('to right',  '#f9ce34', '#ee2a7b', '#6228d7'),
-        padding: 2
-    },
-    storyImageProfile: {
-        width: 70,
-        height: 70,
-        borderRadius: 35
-    },
-    storyUserName: {
-        color: 'black',
-        fontSize: 12,
-        marginTop: 5,
-        textAlign: 'center'
-    }
-})
+  storyContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  storyImageContainer: {
+    borderWidth: 3,
+    borderRadius: 40,
+    borderColor: '#e5009f',
+    padding: 2,
+  },
+  storyImageProfile: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  storyUserName: {
+    color: 'black',
+    fontSize: 12,
+    marginTop: 5,
+    textAlign: 'center',
+    maxWidth: 75, // Membatasi lebar username agar tidak terlalu panjang
+  },
+});
--- src/utils/UsersData.ts
+++ src/utils/UsersData.ts
@@ -1,70 +1,123 @@
+import { date } from "yup";
+
 export const UsersData = [
   {
     id: 1,
-    name: 'Elon Musk',
-    username: 'muskelon',
-    profile: require('../assets/data/elondp.png'),
+    username: 'andkstrr_',
+    name: 'Andika Satrio Nurcahyo',
+    profile: require('../assets/data/andkstrr.jpg'),
     story: {
       time: 10,
-      image: require('../assets/data/elonstory.png'),
+      image: require('../assets/data/andkstrrstory.jpg'),
     },
     post: {
       time: '09:00:00',
-      date: '01/05/2023',
-      image: require('../assets/data/elonpost.png'),
-      caption: 'Hi Everyone, Elon musk is here',
-      like: 30,
+      date: '13/01/2025',
+      image: require('../assets/data/andkstrrpost.jpg'),
+      caption: 'Good things take time',
+      like: 137,
     },
   },
   {
     id: 2,
-    name: 'Harsh Beniwal',
-    username: 'harsh',
-    profile: require('../assets/data/harshdp.png'),
+    username: 'rplwikrama',
+    name: 'RPL - Wikrama Bogor',
+    profile: require('../assets/data/rpl.jpg'),
     story: {
-      time: 8,
-      image: require('../assets/data/harshs.png'),
+      time: 11,
+      image: require('../assets/data/rplstory.png'),
     },
     post: {
-      time: '04:00 PM',
-      date: '08/04/2023',
-      image: require('../assets/data/harshp.png'),
-      caption: 'hi....',
-      like: 25,
-    },
+      time: '13.15.00',
+      date: '12/03/2025',
+      // image: require('../assets/data/rplpost.jpg'),
+      caption: 'wahh keren banget nih!',
+      like: 137,
+    }
   },
   {
     id: 3,
-    name: 'Modi',
-    username: 'nmodi',
-    profile: require('../assets/data/modidp.png'),
+    username: 'baitullahindonesia',
+    name: 'Baitullah',
+    profile: require('../assets/data/baitullah.jpg'),
     story: {
-      time: 15,
-      image: require('../assets/data/modip.png'),
+      time: 11,
+      image: require('../assets/data/baitullahstory.png'),
     },
     post: {
-      time: '07:00 AM',
-      date: '12/05/2023',
-      image: require('../assets/data/modis.png'),
-      caption: '2000 ke note band',
-      like: 99,
-    },
+      time: '13.15.00',
+      date: '12/03/2025',
+      image: require('../assets/data/baitullahpost.png'),
+      caption: 'wahh keren banget nih!',
+      like: 137,
+    }
   },
-  {
-    id: 4,
-    name: 'Sonam',
-    username: 'sonamb12',
-    profile: require('../assets/data/sonamdp.png'),
-    story: {
-      time: 13,
-      image: require('../assets/data/sonams.png'),
-    },
-    post: {
-      time: '07:00 AM',
-      date: '12/05/2023',
-      image: require('../assets/data/sonmp.png'),
-      caption: 'carry on jatta',
-      like: 88,
-    },
-  },
+  // {
+  //   id: 4,
+  //   username: 'baitullahindonesia',
+  //   name: 'Baitullah',
+  //   profile: require('../assets/data/baitullah.jpg'),
+  //   story: {
+  //     time: 11,
+  //     image: require('../assets/data/baitullahstory.png'),
+  //   },
+  //   post: {
+  //     time: '13.15.00',
+  //     date: '12/03/2025',
+  //     image: require('../assets/data/baitullahpost.png'),
+  //     caption: 'Hal-hal unik pas pertama kali umroh!',
+  //     like: 137,
+  //   }
+  // },
+  // {
+  //   id: 5,
+  //   username: 'smkwikrama',
+  //   name: 'SMK Wikrama Bogor | SMK Teladan Nasional',
+  //   profile: require('../assets/data/wikrama.jpg'),
+  //   story: {
+  //     time: 11,
+  //     image: require('../assets/data/wikramastory.png'),
+  //   },
+  //   post: {
+  //     time: '13.15.00',
+  //     date: '8/03/2025',
+  //     image: require('../assets/data/wikramapost.png'),
+  //     caption: 'wahh keren banget nih!',
+  //     like: 137,
+  //   }
+  // },
+  // {
+  //   id: 6,
+  //   username: 'sandhikagalih',
+  //   name: 'Sandhika Galih - Belajar Coding',
+  //   profile: require('../assets/data/sandhika.jpg'),
+  //   story: {
+  //     time: 11,
+  //     image: require('../assets/data/sandhikastory.png'),
+  //   },
+  //   post: {
+  //     time: '13.15.00',
+  //     date: '8/03/2025',
+  //     image: require('../assets/data/sandhikapost.png'),
+  //     caption: 'wahh keren banget nih!',
+  //     like: 137,
+  //   }
+  // },
+  // {
+  //   id: 7,
+  //   username: 'dicoding',
+  //   name: 'Dicoding Indonesia',
+  //   profile: require('../assets/data/dicoding.jpg'),
+  //   story: {
+  //     time: 11,
+  //     image: require('../assets/data/dicodingstory.png'),
+  //   },
+  //   post: {
+  //     time: '13.15.00',
+  //     date: '8/03/2025',
+  //     image: require('../assets/data/dicodingpost.png'),
+  //     caption: 'wahh keren banget nih!',
+  //     like: 137,
+  //   }
+  // }
 ];
