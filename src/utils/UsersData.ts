26c17e8ceb8e8b83fd8c474a276fe7ef01a7f5c5
andkstrr <andikasatrionurcahyo@gmail.com>
2025-03-06 20:54:17 +0700

    update usersdata.ts

--- src/utils/UsersData.ts
+++ src/utils/UsersData.ts
@@ -20,6 +20,23 @@ export const UsersData = [
   },
   {
     id: 2,
+    username: 'smkwikrama',
+    name: 'SMK Wikrama Bogor | SMK Teladan Nasional',
+    profile: require('../assets/data/wikrama.jpg'),
+    story: {
+      time: 11,
+      image: require('../assets/data/wikramastory.png'),
+    },
+    post: {
+      time: '13.15.00',
+      date: '12/03/2025',
+      image: require('../assets/data/wikramapost.png'),
+      caption: 'Tentukan masa depanmu disini!',
+      like: 137,
+    }
+  },
+  {
+    id: 3,
     username: 'rplwikrama',
     name: 'RPL - Wikrama Bogor',
     profile: require('../assets/data/rpl.jpg'),
@@ -30,13 +47,30 @@ export const UsersData = [
     post: {
       time: '13.15.00',
       date: '12/03/2025',
-      // image: require('../assets/data/rplpost.jpg'),
-      caption: 'wahh keren banget nih!',
+      image: require('../assets/data/rplpost.png'),
+      caption: 'Semoga bermanfaat yaa guys!',
+      like: 74,
+    }
+  },
+  {
+    id: 4,
+    username: 'aswad_baitullah',
+    name: 'Aswad | Platform Agent Travel | Agent Mandiri',
+    profile: require('../assets/data/aswad.jpg'),
+    story: {
+      time: 11,
+      image: require('../assets/data/aswadstory.png'),
+    },
+    post: {
+      time: '13.15.00',
+      date: '8/03/2025',
+      image: require('../assets/data/aswadpost.png'),
+      caption: 'Stay tuned, ini bakal ngebantu kamu yang lagi hustle cari kerja!!',
       like: 137,
     }
   },
   {
-    id: 3,
+    id: 5,
     username: 'baitullahindonesia',
     name: 'Baitullah',
     profile: require('../assets/data/baitullah.jpg'),
@@ -48,61 +82,27 @@ export const UsersData = [
       time: '13.15.00',
       date: '12/03/2025',
       image: require('../assets/data/baitullahpost.png'),
+      caption: 'Hal-hal unik pas pertama kali umroh!',
+      like: 137,
+    }
+  },
+  {
+    id: 6,
+    username: 'sandhikagalih',
+    name: 'Sandhika Galih - Belajar Coding',
+    profile: require('../assets/data/sandhika.jpg'),
+    story: {
+      time: 11,
+      image: require('../assets/data/sandhikastory.png'),
+    },
+    post: {
+      time: '13.15.00',
+      date: '8/03/2025',
+      image: require('../assets/data/sandhikapost.png'),
       caption: 'wahh keren banget nih!',
       like: 137,
     }
   },
-  // {
-  //   id: 4,
-  //   username: 'baitullahindonesia',
-  //   name: 'Baitullah',
-  //   profile: require('../assets/data/baitullah.jpg'),
-  //   story: {
-  //     time: 11,
-  //     image: require('../assets/data/baitullahstory.png'),
-  //   },
-  //   post: {
-  //     time: '13.15.00',
-  //     date: '12/03/2025',
-  //     image: require('../assets/data/baitullahpost.png'),
-  //     caption: 'Hal-hal unik pas pertama kali umroh!',
-  //     like: 137,
-  //   }
-  // },
-  // {
-  //   id: 5,
-  //   username: 'smkwikrama',
-  //   name: 'SMK Wikrama Bogor | SMK Teladan Nasional',
-  //   profile: require('../assets/data/wikrama.jpg'),
-  //   story: {
-  //     time: 11,
-  //     image: require('../assets/data/wikramastory.png'),
-  //   },
-  //   post: {
-  //     time: '13.15.00',
-  //     date: '8/03/2025',
-  //     image: require('../assets/data/wikramapost.png'),
-  //     caption: 'wahh keren banget nih!',
-  //     like: 137,
-  //   }
-  // },
-  // {
-  //   id: 6,
-  //   username: 'sandhikagalih',
-  //   name: 'Sandhika Galih - Belajar Coding',
-  //   profile: require('../assets/data/sandhika.jpg'),
-  //   story: {
-  //     time: 11,
-  //     image: require('../assets/data/sandhikastory.png'),
-  //   },
-  //   post: {
-  //     time: '13.15.00',
-  //     date: '8/03/2025',
-  //     image: require('../assets/data/sandhikapost.png'),
-  //     caption: 'wahh keren banget nih!',
-  //     like: 137,
-  //   }
-  // },
   // {
   //   id: 7,
   //   username: 'dicoding',
